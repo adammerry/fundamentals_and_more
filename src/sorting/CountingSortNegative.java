0598ec09571c498326f69678dd3f051f6f341f5a
Adam Merry <adammerry44@gmail.com>
2021-03-13 22:00:09 -0500

    improve logic and syntax

--- src/sorting/CountingSortNegative.java
+++ src/sorting/CountingSortNegative.java
@@ -8,18 +8,16 @@ public class CountingSortNegative {
       System.out.println("null array encountered");
       return;
     }
-    if (ints.length == 0) return;
+    if (ints.length < 2) return;
     // Determine the range of numbers to be sorted. In some situations this may be given or
     // assumed, but here we calculate it as a preliminary step. This does not change the O(n + k)
     // asymptotic runtime.
     int largest = ints[0], smallest = ints[0];
-    for (int i = 1; i < ints.length; i++) {
-      if (ints[i] > largest) largest = ints[i];
-      if (ints[i] < smallest) smallest = ints[i];
+    for (int i : ints) {
+      if (i > largest) largest = i;
+      else if (i < smallest) smallest = i;
     }
-    int[] counts = new int[Math.abs(largest - smallest + 1)];
-    // We can safely increase any value in the array immediately, since the default value for
-    // elements of an int array in Java is 0.
+    int[] counts = new int[largest - smallest + 1];
     for (int i : ints) counts[i - smallest]++;
     for (int i = 0, sortedIdx = 0; i < counts.length; i++)
       for (; counts[i] > 0; counts[i]--, sortedIdx++) ints[sortedIdx] = i + smallest;
