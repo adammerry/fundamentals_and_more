d7b91d8c7136753852076b0bec83732df74b2003
Adam Merry <adammerry44@gmail.com>
2021-03-22 17:38:53 -0400

    improve test structure

--- test/SearchingTests.java
+++ test/SearchingTests.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Map;
@@ -15,31 +16,23 @@ import searching.BFS;
 import searching.DFS;
 
 public class SearchingTests {
+  private GraphAdjacencyListBetter<Integer> graph = new GraphAdjacencyListBetter<>();
+  private GraphAdjacencyListBetter.Node<Integer> node1 = new GraphAdjacencyListBetter.Node<>(15);
+  private GraphAdjacencyListBetter.Node<Integer> node2 = new GraphAdjacencyListBetter.Node<>(75);
+  private GraphAdjacencyListBetter.Node<Integer> node3 = new GraphAdjacencyListBetter.Node<>(12);
+  private GraphAdjacencyListBetter.Node<Integer> node4 = new GraphAdjacencyListBetter.Node<>(22);
+  private GraphAdjacencyListBetter.Node<Integer> node5 = new GraphAdjacencyListBetter.Node<>(19);
+  private GraphAdjacencyListBetter.Node<Integer> node6 = new GraphAdjacencyListBetter.Node<>(74);
+  private GraphAdjacencyListBetter.Node<Integer> node7 = new GraphAdjacencyListBetter.Node<>(11);
+  private GraphAdjacencyListBetter.Node<Integer> node8 = new GraphAdjacencyListBetter.Node<>(29);
+  private GraphAdjacencyListBetter.Node<Integer> node9 = new GraphAdjacencyListBetter.Node<>(88);
+  private GraphAdjacencyListBetter.Node<Integer> node10 = new GraphAdjacencyListBetter.Node<>(3);
+  private GraphAdjacencyListBetter.Node<Integer> node11 = new GraphAdjacencyListBetter.Node<>(4);
+  private GraphAdjacencyListBetter.Node<Integer> node12 = new GraphAdjacencyListBetter.Node<>(1);
+  private BinaryTree<Integer> tree = new BinaryTree<>();
 
-  @Test
-  public void testTraversals() {
-    Traversals t = new Traversals();
-    assertArrayEquals(new Integer[]{4, 2, 8, 5, 9, 10, 1, 6, 3, 7}, t.listInorder().toArray());
-    assertArrayEquals(new Integer[]{1, 2, 4, 5, 8, 9, 10, 3, 6, 7}, t.listPreorder().toArray());
-    assertArrayEquals(new Integer[]{4, 8, 10, 9, 5, 2, 6, 7, 3, 1}, t.listPostorder().toArray());
-    assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, t.listLevelOrder().toArray());
-  }
-
-  @Test
-  public void testBFS() {
-    GraphAdjacencyListBetter<Integer> graph = new GraphAdjacencyListBetter<>();
-    GraphAdjacencyListBetter.Node<Integer> node1 = new GraphAdjacencyListBetter.Node<>(15);
-    GraphAdjacencyListBetter.Node<Integer> node2 = new GraphAdjacencyListBetter.Node<>(75);
-    GraphAdjacencyListBetter.Node<Integer> node3 = new GraphAdjacencyListBetter.Node<>(12);
-    GraphAdjacencyListBetter.Node<Integer> node4 = new GraphAdjacencyListBetter.Node<>(22);
-    GraphAdjacencyListBetter.Node<Integer> node5 = new GraphAdjacencyListBetter.Node<>(19);
-    GraphAdjacencyListBetter.Node<Integer> node6 = new GraphAdjacencyListBetter.Node<>(74);
-    GraphAdjacencyListBetter.Node<Integer> node7 = new GraphAdjacencyListBetter.Node<>(11);
-    GraphAdjacencyListBetter.Node<Integer> node8 = new GraphAdjacencyListBetter.Node<>(29);
-    GraphAdjacencyListBetter.Node<Integer> node9 = new GraphAdjacencyListBetter.Node<>(88);
-    GraphAdjacencyListBetter.Node<Integer> node10 = new GraphAdjacencyListBetter.Node<>(3);
-    GraphAdjacencyListBetter.Node<Integer> node11 = new GraphAdjacencyListBetter.Node<>(4);
-    GraphAdjacencyListBetter.Node<Integer> node12 = new GraphAdjacencyListBetter.Node<>(1);
+  @Before
+  public void setUpGraphAndTree() {
     graph.addNode(node1);
     graph.addNode(node2);
     graph.addNode(node3);
@@ -67,8 +60,6 @@ public class SearchingTests {
     graph.addEdge(node9, node10);
     graph.addEdge(node10, node11);
     graph.addEdge(node10, node12);
-
-    BinaryTree<Integer> tree = new BinaryTree<>();
     tree.insert(0);
     tree.insert(1);
     tree.insert(2);
@@ -78,7 +69,19 @@ public class SearchingTests {
     tree.insert(6);
     tree.insert(7);
     tree.insert(8);
+  }
 
+  @Test
+  public void testTraversals() {
+    Traversals t = new Traversals();
+    assertArrayEquals(new Integer[]{4, 2, 8, 5, 9, 10, 1, 6, 3, 7}, t.listInorder().toArray());
+    assertArrayEquals(new Integer[]{1, 2, 4, 5, 8, 9, 10, 3, 6, 7}, t.listPreorder().toArray());
+    assertArrayEquals(new Integer[]{4, 8, 10, 9, 5, 2, 6, 7, 3, 1}, t.listPostorder().toArray());
+    assertArrayEquals(new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, t.listLevelOrder().toArray());
+  }
+
+  @Test
+  public void testBFS() {
     BFS bfs = new BFS(graph, tree);
 
     assertEquals(new Integer(15), bfs.bfsGraphWithSeenField(15).getData());
@@ -125,58 +128,6 @@ public class SearchingTests {
 
   @Test
   public void testDFS() {
-    GraphAdjacencyListBetter<Integer> graph = new GraphAdjacencyListBetter<>();
-    GraphAdjacencyListBetter.Node<Integer> node1 = new GraphAdjacencyListBetter.Node<>(15);
-    GraphAdjacencyListBetter.Node<Integer> node2 = new GraphAdjacencyListBetter.Node<>(75);
-    GraphAdjacencyListBetter.Node<Integer> node3 = new GraphAdjacencyListBetter.Node<>(12);
-    GraphAdjacencyListBetter.Node<Integer> node4 = new GraphAdjacencyListBetter.Node<>(22);
-    GraphAdjacencyListBetter.Node<Integer> node5 = new GraphAdjacencyListBetter.Node<>(19);
-    GraphAdjacencyListBetter.Node<Integer> node6 = new GraphAdjacencyListBetter.Node<>(74);
-    GraphAdjacencyListBetter.Node<Integer> node7 = new GraphAdjacencyListBetter.Node<>(11);
-    GraphAdjacencyListBetter.Node<Integer> node8 = new GraphAdjacencyListBetter.Node<>(29);
-    GraphAdjacencyListBetter.Node<Integer> node9 = new GraphAdjacencyListBetter.Node<>(88);
-    GraphAdjacencyListBetter.Node<Integer> node10 = new GraphAdjacencyListBetter.Node<>(3);
-    GraphAdjacencyListBetter.Node<Integer> node11 = new GraphAdjacencyListBetter.Node<>(4);
-    GraphAdjacencyListBetter.Node<Integer> node12 = new GraphAdjacencyListBetter.Node<>(1);
-    graph.addNode(node1);
-    graph.addNode(node2);
-    graph.addNode(node3);
-    graph.addNode(node4);
-    graph.addNode(node5);
-    graph.addNode(node6);
-    graph.addNode(node7);
-    graph.addNode(node8);
-    graph.addNode(node9);
-    graph.addNode(node10);
-    graph.addNode(node11);
-    graph.addNode(node12);
-    graph.addEdge(node1, node2);
-    graph.addEdge(node2, node3);
-    graph.addEdge(node3, node4);
-    graph.addEdge(node4, node5);
-    graph.addEdge(node1, node5);
-    graph.addEdge(node2, node4);
-    graph.addEdge(node3, node6);
-    graph.addEdge(node6, node7);
-    graph.addEdge(node7, node8);
-    graph.addEdge(node2, node8);
-    graph.addEdge(node4, node7);
-    graph.addEdge(node1, node7);
-    graph.addEdge(node9, node10);
-    graph.addEdge(node10, node11);
-    graph.addEdge(node10, node12);
-
-    BinaryTree<Integer> tree = new BinaryTree<>();
-    tree.insert(0);
-    tree.insert(1);
-    tree.insert(2);
-    tree.insert(3);
-    tree.insert(4);
-    tree.insert(5);
-    tree.insert(6);
-    tree.insert(7);
-    tree.insert(8);
-
     DFS dfs = new DFS(graph, tree);
 
     assertEquals(new Integer(15), dfs.dfsGraphIterative(15).getData());
