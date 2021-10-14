ff9947c5309b553eeee5214451756603c8c8f1a3
Adam Merry <adammerry44@gmail.com>
2021-10-14 09:36:08 -0700

    fix type parameters and use ArrayList internally

--- src/dataStructures/PriorityQueueArray.java
+++ src/dataStructures/PriorityQueueArray.java
@@ -1,29 +1,34 @@
 package dataStructures;
 
 import java.util.NoSuchElementException;
+import java.util.ArrayList; // Use Java builtin ArrayList instead of my custom ArrayList.
 
-// Implementation of a generic priority queue using an array as the underlying data structure.
+// Implementation of a generic priority queue using an ArrayList as the underlying data structure.
+// An ArrayList is used here instead of an Array because Arrays do not play nicely with generics in
+// Java. However, the logic involved in this implementation is similar to how a priority queue
+// would be implemented using an Array in other languages, or in Java without generics.
 public class PriorityQueueArray<E> {
-  private static final int DEFAULT_SIZE = 10;
-  private int size;
-  private PriorityQueueElement<E>[] elements;
+  private static final int DEFAULT_CAPACITY = 10;
+  private int size, capacity;
+  private ArrayList<PriorityQueueElement> elements;
 
-  public PriorityQueueArray(int capacity) {
-    elements = (PriorityQueueElement<E>[]) ((capacity > 0) ? new PriorityQueueElement[capacity] :
-            new PriorityQueueElement[DEFAULT_SIZE]);
+  public PriorityQueueArray(int customCapacity) {
+    capacity = (customCapacity > 0) ? customCapacity : DEFAULT_CAPACITY;
+    elements = new ArrayList<>(capacity);
+    for (int i = 0; i < capacity; i++) elements.add(null);
     size = 0;
   }
 
-  private class PriorityQueueElement<F> {
-    private F item;
+  private class PriorityQueueElement {
+    private E item;
     private int priority;
 
-    private PriorityQueueElement(F item, int priority) {
+    private PriorityQueueElement(E item, int priority) {
       this.item = item;
       this.priority = priority;
     }
 
-    private F getItem() { return item; }
+    private E getItem() { return item; }
 
     private int getPriority() { return priority; }
 
@@ -31,14 +36,14 @@ public class PriorityQueueArray<E> {
   }
 
   public void insert(E item, int priority) {
-    if (size == elements.length) throw new IllegalStateException("Priority Queue is at capacity");
-    elements[size++] = new PriorityQueueElement<>(item, priority);
+    if (size == capacity) throw new IllegalStateException("Priority Queue is at capacity");
+    elements.set(size++, new PriorityQueueElement(item, priority));
   }
 
   public E getHighestPriority() {
     if (size == 0) throw new NoSuchElementException("Priority Queue empty");
-    PriorityQueueElement<E> highestPriority = elements[0];
-    for (PriorityQueueElement<E> element : elements)
+    PriorityQueueElement highestPriority = elements.get(0);
+    for (PriorityQueueElement element : elements)
       // A smaller number indicates a higher priority value.
       if (element.getPriority() < highestPriority.getPriority()) highestPriority = element;
     return highestPriority.getItem();
@@ -46,23 +51,24 @@ public class PriorityQueueArray<E> {
 
   public E deleteHighestPriority() {
     if (size == 0) throw new NoSuchElementException("Priority Queue empty");
-    int highestPriority = elements[0].getPriority();
-    int highestPriorityIdx = 0;
-    for (int i = 0; i < size; i++) {
-      if (elements[i].getPriority() < highestPriority) {
-        highestPriority = elements[i].getPriority();
+    int highestPriority = elements.get(0).getPriority(), highestPriorityIdx = 0;
+    for (int i = 1; i < size; i++) {
+      if (elements.get(i).getPriority() < highestPriority) {
+        highestPriority = elements.get(i).getPriority();
         highestPriorityIdx = i;
       }
     }
-    E item = elements[highestPriorityIdx].getItem();
-    elements[highestPriorityIdx] = elements[--size];
+    E item = elements.get(highestPriorityIdx).getItem();
+    elements.set(highestPriorityIdx, elements.get(--size));
     return item;
   }
 
   public void changePriority(E item, int newPriority) {
+    // It is necessary to use a counted for loop here, since indices [size] --> [capacity] may
+    // contain deleted items.
     for (int i = 0; i < size; i++) {
-      if (elements[i].getItem().equals(item)) {
-        elements[i].setPriority(newPriority);
+      if (elements.get(i).getItem().equals(item)) {
+        elements.get(i).setPriority(newPriority);
         return;
       }
     }
--- src/dataStructures/PriorityQueueHeap.java
+++ src/dataStructures/PriorityQueueHeap.java
@@ -1,5 +1,6 @@
 package dataStructures;
 
+import java.util.ArrayList;  // Use Java builtin ArrayList instead of my custom ArrayList.
 import java.util.HashMap;
 import java.util.NoSuchElementException;
 
@@ -7,55 +8,56 @@ import java.util.NoSuchElementException;
 // This is probably the best way to implement a priority queue, since all operations can be
 // executed in sub-linear worst-case time.
 public class PriorityQueueHeap<E> {
-  private static final int DEFAULT_SIZE = 10;
-  private PriorityQueueElement<E>[] heap;
+  private static final int DEFAULT_CAPACITY = 10;
+  private ArrayList<PriorityQueueElement> heap;
   // HashMap used for locating indices of queue elements in O(1) time.
   private HashMap<E, Integer> idxMap;
-  private int size;
+  private int size, capacity;
 
-  public PriorityQueueHeap(int capacity) {
-    heap = (PriorityQueueElement<E>[]) ((capacity > 0) ? new PriorityQueueElement[capacity] :
-            new PriorityQueueElement[DEFAULT_SIZE]);
-    idxMap = new HashMap<>();
+  public PriorityQueueHeap(int customCapacity) {
+    capacity = (customCapacity > 0) ? customCapacity : DEFAULT_CAPACITY;
+    heap = new ArrayList<>(capacity);
+    for (int i = 0; i < capacity; i++) heap.add(null);
     size = 0;
+    idxMap = new HashMap<>();
   }
 
-  public class PriorityQueueElement<F> {
-    private F item;
+  public class PriorityQueueElement {
+    private E item;
     private int priority;
 
-    private PriorityQueueElement(F item, int priority) {
+    private PriorityQueueElement(E item, int priority) {
       this.item = item;
       this.priority = priority;
     }
 
-    public F getItem() { return item; }
+    public E getItem() { return item; }
 
     public int getPriority() { return priority; }
   }
 
   public void insert(E item, int priority) {
-    if (size == heap.length) throw new IllegalStateException("Priority Queue is at capacity");
-    PriorityQueueElement<E> elem = new PriorityQueueElement<>(item, priority);
-    heap[size] = elem;
+    if (size == capacity) throw new IllegalStateException("Priority Queue is at capacity");
+    PriorityQueueElement elem = new PriorityQueueElement(item, priority);
+    heap.set(size, elem);
     idxMap.put(item, size);
     siftUp(size);
     size++;
   }
 
-  public PriorityQueueElement<E> getHighestPriority() {
+  public PriorityQueueElement getHighestPriority() {
     if (size == 0) throw new NoSuchElementException("Priority Queue empty");
-    return heap[0];
+    return heap.get(0);
   }
 
-  public PriorityQueueElement<E> deleteHighestPriority() {
+  public PriorityQueueElement deleteHighestPriority() {
     if (size == 0) throw new NoSuchElementException("Priority Queue empty");
-    PriorityQueueElement<E> highestPriority = heap[0];
+    PriorityQueueElement highestPriority = heap.get(0);
     idxMap.remove(highestPriority.getItem());
     size--;
     if (size > 0) {
-      heap[0] = heap[size];
-      idxMap.put(heap[0].getItem(), 0);
+      heap.set(0, heap.get(size));
+      idxMap.put(heap.get(0).getItem(), 0);
       siftDown(0);
     }
     return highestPriority;
@@ -64,8 +66,8 @@ public class PriorityQueueHeap<E> {
   public void changePriority(E item, int newPriority) {
     int idx = idxMap.getOrDefault(item, -1);
     if (idx >= 0) {
-      boolean newPriorityGreater = newPriority > heap[idx].getPriority();
-      heap[idx].priority = newPriority;
+      boolean newPriorityGreater = newPriority > heap.get(idx).getPriority();
+      heap.get(idx).priority = newPriority;
       if (newPriorityGreater) siftDown(idx);
       else siftUp(idx);
     }
@@ -77,13 +79,14 @@ public class PriorityQueueHeap<E> {
   public boolean contains(E item) { return idxMap.containsKey(item); }
 
   public Integer getPriority(E item) {
-    return idxMap.containsKey(item) ? heap[idxMap.get(item)].getPriority() : null;
+    if (idxMap.containsKey(item)) return heap.get(idxMap.get(item)).getPriority();
+    throw new NoSuchElementException("Item not found in Priority Queue");
   }
 
   private void siftUp(int childIdx) {
     if (childIdx > 0) {
       int parentIdx = getParentIdx(childIdx);
-      if (heap[childIdx].getPriority() < heap[parentIdx].getPriority()) {
+      if (heap.get(childIdx).getPriority() < heap.get(parentIdx).getPriority()) {
         swap(childIdx, parentIdx);
         siftUp(parentIdx);
       }
@@ -94,9 +97,9 @@ public class PriorityQueueHeap<E> {
     int leftChildIdx = getLeftChildIdx(parentIdx);
     int rightChildIdx = leftChildIdx + 1;
     int smallest = parentIdx;
-    if (leftChildIdx < size && heap[leftChildIdx].getPriority() < heap[smallest].getPriority())
+    if (leftChildIdx < size && heap.get(leftChildIdx).getPriority() < heap.get(smallest).getPriority())
       smallest = leftChildIdx;
-    if (rightChildIdx < size && heap[rightChildIdx].getPriority() < heap[smallest].getPriority())
+    if (rightChildIdx < size && heap.get(rightChildIdx).getPriority() < heap.get(smallest).getPriority())
       smallest = rightChildIdx;
     if (smallest != parentIdx) {
       swap(parentIdx, smallest);
@@ -106,11 +109,11 @@ public class PriorityQueueHeap<E> {
 
   private void swap(int idx1, int idx2) {
     if (idx1 != idx2) {
-      PriorityQueueElement<E> temp = heap[idx1];
-      heap[idx1] = heap[idx2];
-      heap[idx2] = temp;
-      idxMap.put(heap[idx1].getItem(), idx1);
-      idxMap.put(heap[idx2].getItem(), idx2);
+      PriorityQueueElement temp = heap.get(idx1);
+      heap.set(idx1, heap.get(idx2));
+      heap.set(idx2, temp);
+      idxMap.put(heap.get(idx1).getItem(), idx1);
+      idxMap.put(heap.get(idx2).getItem(), idx2);
     }
   }
 
